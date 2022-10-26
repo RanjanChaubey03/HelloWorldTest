25249dd71ddcd5e58d1147534f7e4e5fb9641526
Ranjan Chaubey <chaubeyranjan8@gmail.com>
2022-10-26 16:15:38 +0530

    updated source code

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
 
 	public static void main(String args[])
 	{
-		System.out.println("Hello World, This is for Jenkins Practice.");
+		System.out.println("Build through jenkins is in progress");
 
 	}
 }
